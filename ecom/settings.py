75b119cdd76d9987cb42b292afda353a37a97811
rodrigo-munguia <rodrigo.munguia@gmail.com>
2023-02-01 13:21:49 -0600

    c17

--- ecom/settings.py
+++ ecom/settings.py
@@ -263,8 +263,9 @@ else:
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
-        "whitenoise.middleware.WhiteNoiseMiddleware",
+        
     ]
+    #"whitenoise.middleware.WhiteNoiseMiddleware",
 
     ROOT_URLCONF = 'ecom.urls'
 
@@ -336,7 +337,7 @@ else:
     ]
     
     #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    #STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
     
 
