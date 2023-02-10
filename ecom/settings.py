9b3a148f4bb6cf6efddd126b3e7501138298fa84
rodrigo-munguia <rodrigo.munguia@gmail.com>
2023-02-09 18:22:26 -0600

    c47

--- ecom/settings.py
+++ ecom/settings.py
@@ -366,23 +366,20 @@ else:
     
      # for railway --------------------------------------------
     ALLOWED_HOSTS = ['*','https://*.up.railway.app/']  # for railway
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    
     #STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
-    #------------------------------------------------------
+    #STATIC_ROOT = BASE_DIR / 'staticfiles'   
     
-    STATIC_URL = 'static/'
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
     
-   
-    
-    #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-    
-
-    
-
     MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')    
+    
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+     
+
+    
 
     CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
