4951c4d91b293224ac256917f56ca539c69d90fc
rodrigo-munguia <rodrigo.munguia@gmail.com>
2023-02-10 10:05:47 -0600

    c57

--- ecom/wsgi.py
+++ ecom/wsgi.py
@@ -15,4 +15,4 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecom.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+#application = WhiteNoise(application)
