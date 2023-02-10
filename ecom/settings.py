ffb5ebeab267451806fe50f7d66803346cf2cb08
rodrigo-munguia <rodrigo.munguia@gmail.com>
2023-02-10 10:58:57 -0600

    c62

--- ecom/settings.py
+++ ecom/settings.py
@@ -375,6 +375,7 @@ else:
     STATIC_URL = '/static/'
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
+        os.path.join(BASE_DIR, 'images'),
         ]
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
     
