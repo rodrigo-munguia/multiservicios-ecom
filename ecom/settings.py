f41768f578d98a6adf964d5a396be714fd387190
rodrigo-munguia <rodrigo.munguia@gmail.com>
2023-02-10 09:00:43 -0600

    c56

--- ecom/settings.py
+++ ecom/settings.py
@@ -379,8 +379,10 @@ else:
     MEDIA_URL = '/images/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'images')    
     
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-     
+    WSGI_APPLICATION = 'gep_project.wsgi.application'
+    
+    #STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage' 
 
     
 
